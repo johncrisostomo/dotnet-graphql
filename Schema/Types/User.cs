7b47e06690ed8c763ceec235bf1b5895d15dd5b7
John Crisostomo <johncrisostomo@protonmail.ch>
2018-06-01 09:27:40 +0800

    Add concrete class off the abstract one

--- Schema/Types/User.cs
+++ Schema/Types/User.cs
@@ -2,8 +2,13 @@ namespace Schema.Types
 {
     public abstract class User
     {
-        public string Id { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
+    }
+
+    public class ProjectUser : User
+    {
+
     }
 }
