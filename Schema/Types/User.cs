fc2b52fd288e0ed13e66a8c192ac1fcd522df807
John Crisostomo <johncrisostomo@protonmail.ch>
2018-06-01 09:19:13 +0800

    Add user abstract class

---
+++ Schema/Types/User.cs
@@ -0,0 +1,9 @@
+namespace Schema.Types
+{
+    public abstract class User
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
